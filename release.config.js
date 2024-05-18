6e1dc48d01a8ec777901b8c4a73259cc8d7faa93
MorveN11 <morales.patty.jose@gmail.com>
2024-05-18 05:16:37 -0400

    ci(*): init ci configurations

---
+++ release.config.js
@@ -0,0 +1,41 @@
+const config = {
+    branches: ["main"],
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                releaseRules: [
+                    { type: "feat", release: "minor" },
+                    { type: "fix", release: "patch" },
+                    { type: "ci", release: "minor" },
+                    { type: "chore", release: "minor" },
+                    { type: "docs", release: "minor" },
+                ],
+            },
+        ],
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/changelog",
+            {
+                changelogFile: "CHANGELOG.md",
+                changelogTitle: "# Changelog\n\n## Mario Built with Pygame\n\n",
+            },
+        ],
+        [
+            "@semantic-release/npm",
+            {
+                npmPublish: false,
+            },
+        ],
+        [
+            "@semantic-release/git",
+            {
+                assets: ["package.json", "CHANGELOG.md"],
+                message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+            },
+        ],
+        "@semantic-release/github",
+    ],
+};
+
+module.exports = config;
