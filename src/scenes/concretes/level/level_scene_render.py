0f1775f622363deb18ab65283d10f0cd982f11d2
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-05-31 16:50:02 -0400

    feat(*): feat: adding manager and changes relate with ticks and scene

--- src/scenes/concretes/level/level_scene_render.py
+++ src/scenes/concretes/level/level_scene_render.py
@@ -1,27 +1,30 @@
-from src.enums import Level, World
-from src.managers import GameManager
-from src.utils.text import get_centered_message
+from src.managers import GameManager, LevelManager
+from src.utils.colors import WHITE_COLOR
+from src.utils.text import get_centered_message, get_format_number, get_message
 
 from ...interfaces import IRender
 
 
 class LevelSceneRender(IRender):
-    def __init__(self, world: World, level: Level) -> None:
+    def __init__(self, level_manager: LevelManager) -> None:
+        self.level_manager = level_manager
         super().__init__()
-        self.world = world
-        self.level = level
 
     def render(self, game_manager: GameManager) -> None:
-        level_data = game_manager.game_data.get_level_data(
-            self.world, self.level
+        game_manager.screen.blit(self.level_manager.background, (0, 0))
+
+        format_time = get_format_number(self.level_manager.current_time)
+        time_message = f"TIME\n {format_time}"
+
+        time, time_rect = get_message(
+            time_message, 1400, 70, text_color=WHITE_COLOR, size=30
         )
-        game_manager.screen.blit(level_data.get_background(), (0, 0))
+        game_manager.screen.blit(time, time_rect)
 
         message = "Playing: Press X or Space to finish"
         text, text_rect = get_centered_message(message)
         game_manager.screen.blit(text, text_rect)
 
-        game_manager.hero.draw(game_manager.screen)
-
-        for manager in game_manager.managers:
+        self.level_manager.hero.draw(game_manager.screen)
+        for manager in self.level_manager.managers:
             manager.draw(game_manager.screen)
--- src/scenes/concretes/level/level_scene_tick.py
+++ src/scenes/concretes/level/level_scene_tick.py
@@ -1,18 +1,32 @@
+from pygame import time
+
 from src.enums import GameEvent, GameState
-from src.managers import GameManager
+from src.managers import GameManager, LevelManager
+from src.utils.constants import INIT_GAME_THRESHOLD, TO_SECONDS
 
 from ...interfaces import ITick
 
 
 class LevelSceneTick(ITick):
-    def __init__(self) -> None:
+    def __init__(self, level_manager: LevelManager) -> None:
+        self.level_manager = level_manager
         super().__init__()
 
     def tick(self, game_manager: GameManager) -> None:
+        seconds_elapsed = (
+            time.get_ticks() - self.level_manager.start_tick
+        ) / TO_SECONDS
+
+        if int(seconds_elapsed) > INIT_GAME_THRESHOLD:
+            self.level_manager.current_time = (
+                self.level_manager.start_time
+                - int(seconds_elapsed)
+                + INIT_GAME_THRESHOLD
+            )
+
         if game_manager.game_events[GameEvent.JUMP]:
             game_manager.game_state = GameState.NEXT_SCENE
 
-        game_manager.hero.update(game_manager.game_events)
-
-        for manager in game_manager.managers:
+        self.level_manager.hero.update(game_manager.game_events)
+        for manager in self.level_manager.managers:
             manager.update()
