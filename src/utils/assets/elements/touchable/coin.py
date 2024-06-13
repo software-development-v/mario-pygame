ac04eec7c99c40b471f01f339dfe531a203c2a3b
denis.ganda <gandarillas.delgado.denis@gmail.com>
2024-06-12 22:55:45 -0400

    feat: add the blocks sprites

---
+++ src/utils/assets/elements/touchable/coin.py
@@ -0,0 +1,9 @@
+from src.utils.assets.players.create_images import create_image
+from src.utils.constants import COIN_SIZE
+from src.utils.directories import COIN_DIR
+
+
+COIN_1 = create_image(COIN_DIR + "1.png", COIN_SIZE)
+COIN_2 = create_image(COIN_DIR + "2.png", COIN_SIZE)
+COIN_3 = create_image(COIN_DIR + "3.png", COIN_SIZE)
+COIN_4 = create_image(COIN_DIR + "4.png", COIN_SIZE)
--- src/utils/assets/elements/touchable/pipe.py
+++ src/utils/assets/elements/touchable/pipe.py
@@ -1,11 +1,11 @@
-from pygame import image, transform
+from src.utils.assets.players.create_images import create_image
+from src.utils.constants import GENERAL_SIZE
+from src.utils.directories import PIPE_BODY_DIR, PIPE_HEAD_DIR
 
-from src.utils.directories import PIPE_DIR
 
-BIG_PIPE = transform.scale(image.load(PIPE_DIR + "big-pipe.png"), (100, 200))
-MEDIUM_PIPE = transform.scale(
-    image.load(PIPE_DIR + "medium-pipe.png"), (100, 150)
-)
-SMALL_PIPE = transform.scale(
-    image.load(PIPE_DIR + "small-pipe.png"), (100, 100)
-)
+PIPE_BODY_STYLE_1 = create_image(PIPE_BODY_DIR + "style_1.png", GENERAL_SIZE)
+PIPE_BODY_STYLE_2 = create_image(PIPE_BODY_DIR + "style_2.png", GENERAL_SIZE)
+PIPE_BODY_STYLE_3 = create_image(PIPE_BODY_DIR + "style_3.png", GENERAL_SIZE)
+PIPE_BODY_STYLE_4 = create_image(PIPE_BODY_DIR + "style_4.png", GENERAL_SIZE)
+
+PIPE_HEAD = create_image(PIPE_HEAD_DIR + "head.png", GENERAL_SIZE)
