74e45154681bc03d42b47d0e9dd0e8d067838cc4
denis.ganda <gandarillas.delgado.denis@gmail.com>
2024-06-21 15:53:19 -0400

    fix: fix sprites

--- src/utils/assets/elements/touchable/block.py
+++ src/utils/assets/elements/touchable/block.py
@@ -1,28 +1,28 @@
-from pygame import image, transform
-
+from src.utils.assets.create_images import create_image
 from src.utils.constants import GENERAL_SIZE
 from src.utils.directories import (
     BRICK_DIR,
-    MYSTERY_BLOCK_DIR,
+    MASTERY_BLOCK_DIR,
     TERRAIN_DIR,
     WALL_DIR,
 )
 
-OVERWORLD_BLOCK = transform.scale(
-    image.load(BRICK_DIR + "overworld.png"), GENERAL_SIZE
+OVERWORLD_BLOCK = create_image(BRICK_DIR + "overworld.png", GENERAL_SIZE)
+
+WALL_BLOCK = create_image(WALL_DIR + "wall.png", GENERAL_SIZE)
+
+MASTERY_BLOCK_1 = create_image(
+    MASTERY_BLOCK_DIR + "mistery_block_1.png", GENERAL_SIZE
 )
 
-
-WALL_BLOCK = transform.scale(image.load(WALL_DIR + "wall.png"), GENERAL_SIZE)
-
-MYSTERY_BLOCK_1 = transform.scale(
-    image.load(MYSTERY_BLOCK_DIR + "mistery_block_1.png"), GENERAL_SIZE
+MASTERY_BLOCK_2 = create_image(
+    MASTERY_BLOCK_DIR + "mistery_block_2.png", GENERAL_SIZE
 )
 
-MYSTERY_BLOCK_2 = transform.scale(
-    image.load(MYSTERY_BLOCK_DIR + "mistery_block_2.png"), GENERAL_SIZE
+MASTERY_BLOCK_3 = create_image(
+    MASTERY_BLOCK_DIR + "mistery_block_3.png", GENERAL_SIZE
 )
 
-OVERWORLD_TERRAIN_BLOCK = transform.scale(
-    image.load(TERRAIN_DIR + "overworld.png"), GENERAL_SIZE
+OVERWORLD_TERRAIN_BLOCK = create_image(
+    TERRAIN_DIR + "overworld.png", GENERAL_SIZE
 )
