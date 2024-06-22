f8c1671dcfe6439c43f7935b1bdf4411641679f9
VictorCespedesCC <victor.cespedes.9374448@usalesiana.edu.bo>
2024-06-21 21:19:07 -0400

    fix: change assets size

--- src/utils/assets/elements/non_touchable/bush.py
+++ src/utils/assets/elements/non_touchable/bush.py
@@ -1,25 +1,28 @@
 from pygame import image, transform
 
-from src.utils.constants import GENERAL_HEIGHT, GENERAL_WIDTH
+from src.utils.constants import BUSH_HEIGHT, BUSH_WIDTH_FACTOR
 from src.utils.directories import BUSH_BACKGROUND_DIR
 
 BIG_BUSH = transform.scale(
     image.load(BUSH_BACKGROUND_DIR + "big_bush.png"),
     (
-        ((GENERAL_WIDTH * 6) - (GENERAL_WIDTH / 6)),
-        ((GENERAL_HEIGHT * 2) + (GENERAL_HEIGHT / 3)),
+        BUSH_HEIGHT * BUSH_WIDTH_FACTOR,
+        BUSH_HEIGHT,
     ),
 )
 
 MEDIUM_BUSH = transform.scale(
     image.load(BUSH_BACKGROUND_DIR + "medium_bush.png"),
     (
-        ((GENERAL_WIDTH * 4) + (GENERAL_WIDTH / 6)),
-        ((GENERAL_HEIGHT * 2) + (GENERAL_HEIGHT / 6)),
+        (BUSH_HEIGHT - 10) * BUSH_WIDTH_FACTOR,
+        BUSH_HEIGHT - 10,
     ),
 )
 
 SMALL_BUSH = transform.scale(
     image.load(BUSH_BACKGROUND_DIR + "small_bush.png"),
-    (GENERAL_WIDTH * 3, ((GENERAL_HEIGHT * 2) + (GENERAL_HEIGHT / 6))),
+    (
+        (BUSH_HEIGHT - 20) * BUSH_WIDTH_FACTOR,
+        BUSH_HEIGHT - 20,
+    ),
 )
--- src/utils/assets/elements/non_touchable/mountain.py
+++ src/utils/assets/elements/non_touchable/mountain.py
@@ -1,28 +1,23 @@
 from pygame import image, transform
 
-from src.utils.constants import GENERAL_HEIGHT, GENERAL_WIDTH
+from src.utils.constants import (
+    BIG_MOUNTAIN_SIZE,
+    MEDIUM_MOUNTAIN_SIZE,
+    SMALL_MOUNTAIN_SIZE,
+)
 from src.utils.directories import MOUNTAIN_BACKGROUND_DIR
 
 BIG_MOUNTAIN = transform.scale(
     image.load(MOUNTAIN_BACKGROUND_DIR + "big_mountain.png"),
-    (
-        ((GENERAL_WIDTH * 7) - (GENERAL_WIDTH / 3)),
-        ((GENERAL_HEIGHT * 5) + (GENERAL_HEIGHT / 3)),
-    ),
+    (BIG_MOUNTAIN_SIZE,),
 )
 
 MEDIUM_MOUNTAIN = transform.scale(
     image.load(MOUNTAIN_BACKGROUND_DIR + "medium_mountain.png"),
-    (
-        ((GENERAL_WIDTH * 6) - (GENERAL_WIDTH / 6)),
-        ((GENERAL_HEIGHT * 4) + (GENERAL_HEIGHT / 6)),
-    ),
+    (MEDIUM_MOUNTAIN_SIZE,),
 )
 
 SMALL_MOUNTAIN = transform.scale(
     image.load(MOUNTAIN_BACKGROUND_DIR + "small_mountain.png"),
-    (
-        ((GENERAL_WIDTH * 4) + (GENERAL_WIDTH / 6)),
-        ((GENERAL_HEIGHT * 2) + (GENERAL_HEIGHT / 12)),
-    ),
+    (SMALL_MOUNTAIN_SIZE,),
 )
