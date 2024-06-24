7d6bf45aaaad5b82a0d1eb74e9b516e95e42152f
VictorCespedesCC <victor.cespedes.9374448@usalesiana.edu.bo>
2024-06-24 11:38:26 -0400

    fix: solve problems caused by merges conflics

--- src/utils/assets/elements/non_touchable/mountain.py
+++ src/utils/assets/elements/non_touchable/mountain.py
@@ -9,15 +9,15 @@ from src.utils.directories import MOUNTAIN_BACKGROUND_DIR
 
 BIG_MOUNTAIN = transform.scale(
     image.load(MOUNTAIN_BACKGROUND_DIR + "big_mountain.png"),
-    (BIG_MOUNTAIN_SIZE,),
+    (BIG_MOUNTAIN_SIZE),
 )
 
 MEDIUM_MOUNTAIN = transform.scale(
     image.load(MOUNTAIN_BACKGROUND_DIR + "medium_mountain.png"),
-    (MEDIUM_MOUNTAIN_SIZE,),
+    (MEDIUM_MOUNTAIN_SIZE),
 )
 
 SMALL_MOUNTAIN = transform.scale(
     image.load(MOUNTAIN_BACKGROUND_DIR + "small_mountain.png"),
-    (SMALL_MOUNTAIN_SIZE,),
+    (SMALL_MOUNTAIN_SIZE),
 )
--- src/utils/assets/elements/non_touchable/tree.py
+++ src/utils/assets/elements/non_touchable/tree.py
@@ -5,15 +5,15 @@ from src.utils.directories import TREE_BACKGROUND_DIR
 
 BIG_TREE = transform.scale(
     image.load(TREE_BACKGROUND_DIR + "big_tree.png"),
-    (BIG_TREE_SIZE,),
+    (BIG_TREE_SIZE),
 )
 
 MEDIUM_TREE = transform.scale(
     image.load(TREE_BACKGROUND_DIR + "medium_tree.png"),
-    (MEDIUM_TREE_SIZE,),
+    (MEDIUM_TREE_SIZE),
 )
 
 SMALL_TREE = transform.scale(
     image.load(TREE_BACKGROUND_DIR + "small_tree.png"),
-    (SMALL_TREE_SIZE,),
+    (SMALL_TREE_SIZE),
 )
