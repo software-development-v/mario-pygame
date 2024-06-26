333b99143f89cfb960e1e0437663984e4f881451
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-06-26 02:03:17 -0400

    feat(*): adding imports to call one time

--- src/utils/assets/backgrounds/menu.py
+++ src/utils/assets/backgrounds/menu.py
@@ -3,3 +3,6 @@ from pygame import image
 from ...directories import BACKGROUNDS_DIR
 
 MENU_BACKGROUND = image.load(BACKGROUNDS_DIR + "menu_back.png")
+CHARACTER_SELECTION_MENU = image.load(
+    BACKGROUNDS_DIR + "character_selector_menu.png"
+)
