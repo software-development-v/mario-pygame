b858aba31dfb59506b99889420388254710dec27
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-06-26 23:29:13 -0400

    feat(*): update score observer to manage and update json

--- src/level/concretes/entity_observers/score_observer.py
+++ src/level/concretes/entity_observers/score_observer.py
@@ -1,14 +1,22 @@
 from src.entities import IElementObserver
 from src.utils import SCORE_LIMIT
+from src.utils.high_score_manager import read_high_score, write_high_score
 
 
 class ScoreObserver(IElementObserver):
     def __init__(self, score: int = 0) -> None:
         self.__score = score
+        self.__high_score = read_high_score()
 
     def update(self, value: int) -> None:
         if self.__score < SCORE_LIMIT:
             self.__score += value
+        if self.__score > self.__high_score:
+            self.__high_score = self.__score
+            write_high_score(self.__high_score)
 
     def get_value(self) -> int:
         return self.__score
+
+    def get_high_score(self) -> int:
+        return self.__high_score
