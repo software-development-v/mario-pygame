9d73ceb52d2b4020c57d6f916e3b217168bcd6c7
Daniel Espinoza <daniel.espinoza.8812451@usalesiana.edu.bo>
2024-07-01 23:14:45 -0400

    refactor: utils imports improved

--- src/entities/concretes/animations/collected_coin.py
+++ src/entities/concretes/animations/collected_coin.py
@@ -1,17 +1,16 @@
-from src.utils.constants import GENERAL_HEIGHT
 from ...abstractions.animation import Animation
 from src.enums import ElementSubType,ElementType
-from src.utils import Position, elements
+from src.utils import Position, elements,GENERAL_HEIGHT
 
 
 class CollectedCoin(Animation):
-    def __init__(self, position: Position, height: float=GENERAL_HEIGHT*2):
+    def __init__(self, position: Position):
         position.y-=GENERAL_HEIGHT
         super().__init__(
             elements[ElementType.COIN][ElementSubType.COIN],
             transitions=[
                 position,
-                Position(position.x, int(position.y - height)),
+                Position(position.x, int(position.y - (GENERAL_HEIGHT*2))),
                 position,
             ],
             animation_interval=20,
