fcacc9cb025ca574bf7c3c7372ff64ecdbd1aa28
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-07-04 00:52:26 -0400

    feat(*): modifications related with concrete enemies

--- src/entities/concretes/enemies/evoomba.py
+++ src/entities/concretes/enemies/evoomba.py
@@ -1,15 +1,18 @@
 from src.enums import EnemyState, EnemyType
 from src.utils import Position, enemies
-
 from ...abstractions.enemy import Enemy
+from src.utils import ANIMATION_INTERVAL, Position
 
 
 class Evoomba(Enemy):
     def __init__(self, position: Position):
         super().__init__(
-            enemies[EnemyType.EVOOMBA],
-            position,
-            EnemyState.WALKING,
+            surfaces=enemies[EnemyType.EVOOMBA],
+            position=position,
+            initial_state=EnemyState.WALKING,
+            animation_interval=ANIMATION_INTERVAL,
+            x_rect_percent=1,
+            y_rect_percent=1,
         )
 
     def update_state(self):
--- src/entities/concretes/enemies/valvoopa.py
+++ src/entities/concretes/enemies/valvoopa.py
@@ -1,5 +1,5 @@
 from src.enums import EnemyState, EnemyType
-from src.utils import Position, enemies
+from src.utils import Position, enemies, ANIMATION_INTERVAL
 
 from ...abstractions.enemy import Enemy
 
@@ -7,9 +7,12 @@ from ...abstractions.enemy import Enemy
 class Valvoopa(Enemy):
     def __init__(self, position: Position):
         super().__init__(
-            enemies[EnemyType.VALVOOPA],
-            position,
-            EnemyState.WALKING,
+            surfaces=enemies[EnemyType.VALVOOPA],
+            position=position,
+            initial_state=EnemyState.WALKING,
+            animation_interval=ANIMATION_INTERVAL,
+            x_rect_percent=1,
+            y_rect_percent=1,
         )
 
     def update_state(self):
