38ffa5fa0a8efa14ef9a683a284b7c0a28c08ede
JeferssonCL <jefersson.coronel.13622689@usalesiana.edu.bo>
2024-07-11 03:22:36 -0400

    refactor(*): update enemy factory and manager

--- src/level/sprites/enemy_manager.py
+++ src/level/sprites/enemy_manager.py
@@ -1,13 +1,19 @@
 from typing import List
 
-from src.entities import Sprite
+from src.utils.camera import Camera
 
+from ...entities import Element, IEnemy
 from ..abstractions import SpritesManager
 
 
-class EnemyManager(SpritesManager[Sprite]):
+class EnemyManager(SpritesManager[IEnemy]):
     def __init__(
         self,
-        enemies: List[Sprite],
+        enemies: List[IEnemy],
     ) -> None:
+        self.__enemies = enemies
         super().__init__(enemies)
+
+    def update(self, camera: Camera, obstacles: List[Element]):
+        for enemy in self.__enemies:
+            enemy.update(obstacles, self.__enemies, camera)
